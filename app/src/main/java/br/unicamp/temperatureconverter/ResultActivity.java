590fa589d19de2ffdc5ff6ed1acf4c4e32cd0b22
Nouani <nouanigabriel@hotmail.com>
2020-08-24 15:44:02 -0300

    Exibição dos resultados

--- app/src/main/java/br/unicamp/temperatureconverter/ResultActivity.java
+++ app/src/main/java/br/unicamp/temperatureconverter/ResultActivity.java
@@ -3,9 +3,13 @@ package br.unicamp.temperatureconverter;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ResultActivity extends AppCompatActivity {
+    EditText edtFahrenheit;
+    EditText edtKelvin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,14 @@ public class ResultActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+
+        this.edtFahrenheit = (EditText)findViewById(R.id.edtFahrenheit);
+        this.edtKelvin = (EditText)findViewById(R.id.edtKelvin);
+
+        Intent intent = getIntent();
+        Bundle input = intent.getExtras();
+
+        edtFahrenheit.setText("" + input.getDouble("fahrenheit"));
+        edtKelvin.setText("" + input.getDouble("kelvin"));
     }
 }
