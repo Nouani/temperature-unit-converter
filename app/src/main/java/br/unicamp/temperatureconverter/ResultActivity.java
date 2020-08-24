ec2c838f80310a7a09a5a703e4b64fd02a2c243a
Nouani <nouanigabriel@hotmail.com>
2020-08-24 15:56:42 -0300

    Botão de voltar na tela de resultados

--- app/src/main/java/br/unicamp/temperatureconverter/ResultActivity.java
+++ app/src/main/java/br/unicamp/temperatureconverter/ResultActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ResultActivity extends AppCompatActivity {
     EditText edtFahrenheit;
     EditText edtKelvin;
+    Button btnReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +22,21 @@ public class ResultActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
 
-        this.edtFahrenheit = (EditText)findViewById(R.id.edtFahrenheit);
-        this.edtKelvin = (EditText)findViewById(R.id.edtKelvin);
+        this.edtFahrenheit = (EditText) findViewById(R.id.edtFahrenheit);
+        this.edtKelvin = (EditText) findViewById(R.id.edtKelvin);
+        this.btnReturn = (Button) findViewById(R.id.btnReturn);
 
         Intent intent = getIntent();
         Bundle input = intent.getExtras();
 
         edtFahrenheit.setText("" + input.getDouble("fahrenheit"));
         edtKelvin.setText("" + input.getDouble("kelvin"));
+
+        this.btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
