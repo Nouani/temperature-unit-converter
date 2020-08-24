fbc2c101406a2379fe1cde905f53109e224f2ba9
Nouani <nouanigabriel@hotmail.com>
2020-08-24 15:11:10 -0300

    Estilização tela de resultados

---
+++ app/src/main/java/br/unicamp/temperatureconverter/ResultActivity.java
@@ -0,0 +1,18 @@
+package br.unicamp.temperatureconverter;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+    }
+}
